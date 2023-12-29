ca436272038049c6a7f9c2f1a05d5364e0cf7496
toshit111 <142931155+toshit111@users.noreply.github.com>
2023-12-29 17:48:42 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Toshit Firake",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "toshitfirake2003@gmail.com",
+  gitHub: "https://github.com/toshit111",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/toshit-firake-7b3163282",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
