38c301b23f0a7b0ce8dc3ae8022c57cb5ac2b892
toshit111 <142931155+toshit111@users.noreply.github.com>
2023-12-29 17:48:42 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/wall.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "background image of brown wall";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at Savitribai Phule Pune University. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "Web designing",
+  "Data Structure and Algorithms",
+  "Problem Solving",
   "Focus group testing",
-  "Mobile user interfaces",
+  "Gaming",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Passionate Computer Science Engineering student | Enthusiastic about coding, AI, and software development | Seeking opportunities to learn and contribute in the tech world";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/study.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "laptop image with some person";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Potfolio WebPage",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Easy and Effective Way to Create Effective Porfolio.",
+    url: "https://github.com/toshit111/OIBSIP.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Physics Tools Webpage",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Contains Physics tools like tempreture conversion",
+    url: "https://github.com/toshit111/OIBSIP.git",
   },
   {
-    title: "My Resume Site",
+    title: "Landing Page",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Best and Simple Project For Web Developer Beginers.",
+    url: "https://github.com/toshit111/OIBSIP.git",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "TO-DO Webapp",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Webapp Contains Daily Routins To-Do with remainder.",
+    url: "https://github.com/toshit111/OIBSIP.git",
   },
 ];
 
